ea9ee173fac40fce42ec7dbd8608aa13e1307c3f
cosmosu <livytence@gmail.com>
2023-03-26 15:38:28 -0400

    add math cap to booty

--- bot.py
+++ bot.py
@@ -13,8 +13,18 @@ token = os.environ.get("TOKEN")
 #create bot client
 client = discord.Client(intents=intents)
 
-def on_ready():
-    print(f"{client.user.name} logged in")
+@client.event 
+async def on_ready():
+        print(f"{client.user.name} logged in")
+
+@client.event
+async def on_message(message):
+      if message.author.bot:
+             return
+      if message.content.startswith("!"):
+        cleanMsg = message.content[1:]
+        await message.channel.send("ew it's you again :rolling_eyes: :raised_hand: " + cleanMsg + "'")
+        
 
 #log in
 client.run(token)
